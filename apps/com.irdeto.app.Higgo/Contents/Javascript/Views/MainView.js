1299e37dcc8936d81b5f32535ce8799f9bc8a9c3
Tianyi Jiao <jiao.tianyi@irdeto.com>
2017-09-20 00:21:19 +0200

    try to fix parse issue

--- apps/com.irdeto.app.Higgo/Contents/Javascript/Views/MainView.js
+++ apps/com.irdeto.app.Higgo/Contents/Javascript/Views/MainView.js
@@ -10,6 +10,7 @@ var MainView = new MAF.Class({
   iotListener: function (evt) {
     if (evt.payload.e && evt.payload.e === "higgo") {
       var data = JSON.stringify(evt.payload.meta.messages[0]);
+      screen.log(data);
       this.updateView(data);
     }
   },
@@ -111,7 +112,8 @@ var MainView = new MAF.Class({
   },
 
   updateView: function (data) {
-    screen.log(JSON.stringify(data));
+    if (data)
+      screen.log(JSON.parse(data.split("\'").join("\"")));
 
     // var parsed_data = JSON.parse(data);
 
